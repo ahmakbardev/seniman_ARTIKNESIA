f644d191b49d930995570114186e0342e7c28493
Ahmad Akbar Mubarak <130969627+ahmakbardev@users.noreply.github.com>
2024-05-15 15:25:45 +0700

    adding preload on load styles files

--- resources/views/seniman/layouts/layout.blade.php
+++ resources/views/seniman/layouts/layout.blade.php
@@ -7,7 +7,10 @@
     <title>ARTIKNESIA | @stack('title')</title>
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800&display=swap" rel="stylesheet" />
     <link rel="shortcut icon" href="{{ asset('assets/images/logo/logo-square.png') }}" type="image/x-icon">
-    <link rel="stylesheet" href="{{ asset('css/app.css') }}" />
+    <!-- Append version number to CSS file name -->
+    <link rel="stylesheet" href="{{ asset('css/app.css?v=1.02') }}">
+    <!-- Add cache-control headers for CSS and JavaScript files -->
+    <link rel="preload" href="{{ asset('css/app.css?v=1.02') }}" as="style" crossorigin="anonymous" />
     {{-- <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.x.x/dist/alpine.min.js" defer></script> --}}
     <script src="{{ asset('seniman/assets/js/init-alpine.js') }}"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.3/Chart.min.css" />
@@ -23,8 +26,7 @@
 
 
 <body>
-    <div class="flex h-screen bg-gray-900 font-montserrat"
-        :class="{ 'overflow-hidden': isSideMenuOpen }">
+    <div class="flex h-screen bg-gray-900 font-montserrat" :class="{ 'overflow-hidden': isSideMenuOpen }">
         {{-- <div class="notifications absolute top-20 right-5 flex flex-col gap-1">
         <div class=" p-3 bg-green-400/50 border border-green-500 text-white rounded-md">aa</div>
         <div class=" p-3 bg-green-400/50 border border-green-500 text-white rounded-md">aa</div>
