c3e7f65b6fbd109fbf9c9ca316996960040b1b9b
Ahmad Akbar Mubarak <130969627+ahmakbardev@users.noreply.github.com>
2024-09-18 17:33:02 +0700

    add payment method for registering seniman

--- resources/views/seniman/layouts/layout.blade.php
+++ resources/views/seniman/layouts/layout.blade.php
@@ -23,9 +23,9 @@
 
     {{-- @env('production') --}}
         <!-- Append version number to CSS file name -->
-        <link rel="stylesheet" href="{{ asset('css/app.css?v=1.04') }}">
+        <link rel="stylesheet" href="{{ asset('css/app.css?v=1.05') }}">
         <!-- Add cache-control headers for CSS and JavaScript files -->
-        <link rel="preload" href="{{ asset('css/app.css?v=1.04') }}" as="style" crossorigin="anonymous"/>
+        <link rel="preload" href="{{ asset('css/app.css?v=1.05') }}" as="style" crossorigin="anonymous"/>
     {{-- @endenv
 
     @env('local')
